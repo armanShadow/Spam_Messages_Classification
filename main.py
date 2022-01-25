b2d4fb06e0464bfb37f81efab17c57c863531a97
unknown <vaziri.aroo1377@gmail.com>
2022-01-26 01:37:07 +0330

    :sparkles: Logistic Regression Model: softmax hypothesis function

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import re
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -11,6 +10,23 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, confusion_matrix, recall_score
+
+
+class MyModel(tf.keras.Model):
+    def __init__(self, units, input_dim):
+        super().__init__()
+        self.dense1 = tf.keras.layers.Dense(1024, activation='elu', input_dim=input_dim, kernel_initializer='normal')
+        self.dense2 = tf.keras.layers.Dense(256, activation='elu', kernel_initializer='normal')
+        self.dense3 = tf.keras.layers.Dense(64, activation='elu', kernel_initializer='normal')
+        self.dense4 = tf.keras.layers.Dense(16, activation='elu', kernel_initializer='normal')
+        self.dense5 = tf.keras.layers.Dense(units=units, activation='softmax')
+
+    def call(self, inputs):
+        x = self.dense1(inputs)
+        x = self.dense2(x)
+        x = self.dense3(x)
+        x = self.dense4(x)
+        return self.dense5(x)
 
 
 data = pd.read_csv('./data/spam.csv', encoding='latin-1').iloc[:, :2].values
@@ -43,7 +59,21 @@ tfidf = TfidfTransformer()
 X = sentence
 X = cv.fit_transform(X).toarray()
 y = tf.keras.utils.to_categorical(target, 2)
+X = tfidf.fit_transform(X).toarray()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=12)
 
+model = MyModel(2, X_train.shape[1])
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=10, batch_size=60)
 
+y_pred = model.predict(X_test)
+Y_pred=np.argmax(y_pred,axis=1)
+Y_test=np.argmax(y_test,axis=1)
+
+ac = accuracy_score(Y_pred, Y_test)
+cm = confusion_matrix(Y_pred, Y_test)
+rs = recall_score(Y_pred, Y_test)
+
+print(f"Confusion Matrix For Test Dataset:\n {cm} \n Accuracy For Test Dataset: {ac} \n "
+      f"Recall Score For Test Dataset: {rs}")
