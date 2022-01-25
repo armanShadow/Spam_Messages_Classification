01cfb1aa0006c482fa2f8129b3401b77250f5fdf
unknown <vaziri.aroo1377@gmail.com>
2022-01-26 01:46:58 +0330

    :recycle:

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -7,12 +8,12 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.metrics import accuracy_score, confusion_matrix, recall_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score, confusion_matrix, recall_score
 
 
-class MyModel(tf.keras.Model):
+class LogisticRegressionModel(tf.keras.Model):
     def __init__(self, units, input_dim):
         super().__init__()
         self.dense1 = tf.keras.layers.Dense(1024, activation='elu', input_dim=input_dim, kernel_initializer='normal')
@@ -29,51 +30,57 @@ class MyModel(tf.keras.Model):
         return self.dense5(x)
 
 
-data = pd.read_csv('./data/spam.csv', encoding='latin-1').iloc[:, :2].values
+def dataVisualization(target):
+    # data visualization
+    unique, count = np.unique(target, return_counts=True)
+    plt.xticks(unique, unique)
+    plt.bar(unique, count, color=['green', 'blue'])
+    plt.title("True Plot")
+    plt.xlabel("Spam")
+    plt.ylabel("Count")
+    plt.show()
 
-le = LabelEncoder()
-target = le.fit_transform(data[:, 0])
 
-# data visualization
-unique, count = np.unique(target, return_counts=True)
-plt.xticks(unique, unique)
-plt.bar(unique, count, color=['green', 'blue'])
-plt.title("True Plot")
-plt.xlabel("Spam")
-plt.ylabel("Count")
-plt.show()
+def dataPreprocessing(data, target):
+    Stopwords = stopwords.words('english')
+    stemmer = PorterStemmer()
+    email = data[:, 1]
+    email = [re.sub("[^a-zA-Z]", " ", e) for e in email]
+    strings = np.char.split(np.char.lower(email))
+    words = [[stemmer.stem(word) for word in string if word not in set(Stopwords)] for string in strings]
+    sentence = [' '.join(row) for row in words]
+    cv = CountVectorizer()
+    tfidf = TfidfTransformer()
+    X = sentence
+    X = cv.fit_transform(X).toarray()
+    y = tf.keras.utils.to_categorical(target, 2)
+    X = tfidf.fit_transform(X).toarray()
+    return train_test_split(X, y, test_size=0.20, random_state=12)
 
-print('sentence quantity: {} ,train sample: {} ,test sample: {}'.format(len(target), len(target) * 0.8,
-                                                                        len(target) * 0.2))
 
-Stopwords = stopwords.words('english')
-stemmer = PorterStemmer()
-email = data[:, 1]
-email = [re.sub("[^a-zA-Z]", " ", e) for e in email]
-strings = np.char.split(np.char.lower(email))
-words = [[stemmer.stem(word) for word in string if word not in set(Stopwords)] for string in strings]
-sentence = [' '.join(row) for row in words]
+def spamMessageClassification(data):
+    le = LabelEncoder()
+    target = le.fit_transform(data[:, 0])
+    print('sentence quantity: {} ,train sample: {} ,test sample: {}'.format(len(target), len(target) * 0.8,
+                                                                            len(target) * 0.2))
+    dataVisualization(target)
+    X_train, X_test, y_train, y_test = dataPreprocessing(data, target)
 
-cv = CountVectorizer()
-tfidf = TfidfTransformer()
-X = sentence
-X = cv.fit_transform(X).toarray()
-y = tf.keras.utils.to_categorical(target, 2)
-X = tfidf.fit_transform(X).toarray()
+    model = LogisticRegressionModel(2, X_train.shape[1])
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(X_train, y_train, epochs=10, batch_size=60)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=12)
+    y_pred = model.predict(X_test)
+    Y_pred = np.argmax(y_pred, axis=1)
+    Y_test = np.argmax(y_test, axis=1)
 
-model = MyModel(2, X_train.shape[1])
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(X_train, y_train, epochs=10, batch_size=60)
+    ac = accuracy_score(Y_pred, Y_test)
+    cm = confusion_matrix(Y_pred, Y_test)
+    rs = recall_score(Y_pred, Y_test)
 
-y_pred = model.predict(X_test)
-Y_pred=np.argmax(y_pred,axis=1)
-Y_test=np.argmax(y_test,axis=1)
+    print(f"Confusion Matrix For Test Dataset:\n {cm} \n Accuracy For Test Dataset: {ac} \n "
+          f"Recall Score For Test Dataset: {rs}")
 
-ac = accuracy_score(Y_pred, Y_test)
-cm = confusion_matrix(Y_pred, Y_test)
-rs = recall_score(Y_pred, Y_test)
 
-print(f"Confusion Matrix For Test Dataset:\n {cm} \n Accuracy For Test Dataset: {ac} \n "
-      f"Recall Score For Test Dataset: {rs}")
+if __name__ == '__main__':
+    spamMessageClassification(pd.read_csv('./data/spam.csv', encoding='latin-1').iloc[:, :2].values)
